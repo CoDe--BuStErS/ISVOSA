7cf63490783ccdd00b090b27d87008072b02d06d
dobias13 <dobias13@uniba.sk>
2015-01-30 15:23:05 +0100

    Pridanie suborov ktore sme testovali s bugom

---
+++ finalna verzia - buggnuta verzia/agilent_SCPI.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+
+import socket
+
+
+class agilent_SCPI :
+
+    host_ip = None
+    scpi_port = None
+    connection = None
+
+    def __init__(self) :
+        self.host_ip = '192.168.1.102'
+        self.scpi_port = 5025
+        
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((self.host_ip, self.scpi_port))
+        #self.connection.setblocking(0)
+        #self.connection.settimeout(0.3)
+
+
+    def sendCmd(self, command) :
+        #print 'cmd> ' + command
+        self.connection.sendall(command + '\n')
+
+
+    def readData(self) :
+        data = ''
+        while 1:
+            buf = self.connection.recv(1024)
+            data += buf
+            if len(buf) > 1:
+                if buf[-1] == '\n':
+                    break
+            else:
+                break
+        data = data[:-1]
+        #print 'dat> ' + data
+        return data
+
+
+    def readErr(self) :
+        self.connection.sendall(':syst:err?')
+        return self.readData()
+
+
+    def __del__(self) :
+        self.connection.close()
+
+
+###############################################################################
+
+
+if __name__ == '__main__':
+
+    a = agilent_SCPI()
+    a.send_cmd(':syst:pres') # ISVOSA
+    a.send_cmd('*idn?')  # ISVOSA
+    a.sendCmd(':sens1:swe:poin 1601; *wai')
+    a.sendCmd(':sens1:swe:poin?')
+    print a.readData()
+    print a.read_err() # ISVOSA
+    del a
+
+
---
+++ finalna verzia - buggnuta verzia/main.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+
+
+import string
+import sys
+import math
+import time
+
+import agilent, rlcparams
+
+from Scientific.Functions.LeastSquares import leastSquaresFit
+
+def lorentz(params, x): #(y0, A, w, xc) -> y0 + (2*A/math.pi)*(w/(4*(x-xc)**2 + w**2))
+    return params[0] + (2.0*params[1]/math.pi) * ( params[2]/(4.0*(x-params[3])**2 + params[2]**2) )
+
+
+def main(args) :
+
+    saveParamsFile = None
+    saveDataFile = None
+    if len(args) == 2:
+        saveParamsFile = args[1]
+    if len(args) == 3:
+        saveParamsFile = args[1]
+        saveDataFile = args[2]
+         
+    a = agilent.agilent()
+    p = rlcparams.RLCparams()     
+
+    a.setFrequency(center = '8000000', span = '2000000')
+    #print a.frequency['start']
+    #print a.frequency['stop']
+    a.measure()
+    a.plotData()
+    #a.saveData('x.dat')
+
+    m = max(a.data['admitance'])
+    maxIndex = a.data['admitance'].index(m)
+    a.setFrequency(center = '%f' % a.data['frequency'][maxIndex])
+
+    a.setFrequency(span = '1000000')
+
+    a.measure()
+    a.plotData()
+    #a.saveData('x.dat')
+
+    m = max(a.data['admitance'])
+    maxIndex = a.data['admitance'].index(m)
+    a.setFrequency(center = '%f' % a.data['frequency'][maxIndex])
+
+    a.setFrequency(span = '40000')
+    #a.setFrequency(span = '25 KHz')
+    #a.setFrequency(span = '10 KHz')
+
+#    timeStart = time.time()
+
+
+    print 'f\t\tR1\t\tC0\t\t\tC1\t\t\tL1'
+    print 100 * '-'
+
+    while 1 :
+        a.measure()
+        #a.plotData() # plot spectrum
+        if saveDataFile:
+            a.saveData(saveDataFile)
+
+        p.data(a.data['frequency'], a.data['Z'])
+        p.fit()
+        #p.plot() # plot fit
+
+        print str(p.fResonanceFit) + '\t' + str(p.R1) + '\t' + str(p.C0) + '\t' + str(p.C1) + '\t' + str(p.L1)
+        if saveParamsFile:
+            p.saveParams(saveParamsFile)
+            #p.plotParams(saveParamsFile)
+            p.plotAll(saveParamsFile)
+        
+        
+        #print 'C0 =', p.C0
+        #print 'C1 =', p.C1
+        #print 'L1 =', p.L1
+        #print 'Fres =',
+
+    del a
+
+
